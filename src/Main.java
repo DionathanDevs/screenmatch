d7506fc711e0d2340521d3ae480fe11d3ef75843
Dionathan Boing Mesquita <dionathandev@gmail.com>
2025-05-12 23:37:01 -0300

    Metods Iniciais

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Movie m1 = new Movie("Filme 1", 2005,true,0, 256);
+        m1.rating(5.0);
+        m1.rating(5.0);
+        m1.viewTechnical();
+    }
+}
---
+++ src/Movie.java
@@ -0,0 +1,82 @@
+import javax.xml.transform.Source;
+
+public class Movie {
+
+    private String name;
+    private int year;
+    private boolean sessionPlus;
+    private double review;
+    private int totalReviews;
+    private int durationMinutes;
+
+    public Movie(String name, int year, boolean sessionPlus, double review, int durationMinutes) {
+        this.name = name;
+        this.year = year;
+        this.sessionPlus = sessionPlus;
+        this.review = review;
+        this.durationMinutes = durationMinutes;
+    }
+
+    public void viewTechnical(){
+        System.out.println("Nome do filme: " + getName());
+        System.out.println("Year do Filme: " + getYear());
+        System.out.println("Session Plus: " + getSessionPlus());
+        System.out.println("Review: " + getReview());
+        System.out.println("Total Reviews: " + getTotalReviews());
+        System.out.println("Duration Minutes: " + getDurationMinutes());
+        System.out.println("Média Filme: " + averageRating());
+    }
+
+    public void rating(double rating){
+        review += rating;
+        totalReviews++;
+    }
+
+    double averageRating(){
+        return review/totalReviews;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setReview(double review) {
+        this.review = review;
+    }
+
+    public void setDurationMinutes(int durationMinutes) {
+        this.durationMinutes = durationMinutes;
+    }
+    public void setSessionPlus(boolean sessionPlus) {
+        this.sessionPlus = sessionPlus;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public double getReview() {
+        return review;
+    }
+    public int getDurationMinutes() {
+        return durationMinutes;
+    }
+    public boolean getSessionPlus() {
+        return sessionPlus;
+    }
+    public int getTotalReviews() {
+        return totalReviews;
+    }
+    public int getYear() {
+        return year;
+    }
+
+}
+
+
+
+
+
